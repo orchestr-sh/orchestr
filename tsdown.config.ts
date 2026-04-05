0ce1fcc74d98e7db5eaa8c85d477316c7537de22
Stewart Marsh <sweptsquash@live.co.uk>
2026-04-06 00:51:36 +0100

    fix: release workflow
    fix: tsdown entry points
    
    fix: bin orchestr extension

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
+  entry: ['./src/index.ts', './src/Facades/index.ts', './src/Events/index.ts', './src/Console/orchestr.ts'],
   dts: true,
   minify: true,
   unbundle: true,
